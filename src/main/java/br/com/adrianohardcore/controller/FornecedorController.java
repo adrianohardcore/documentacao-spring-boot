289f2a435332b8b0edcb71b3171ef4294b6264ca
Adrino Faria Alves <adriano.faria@gmail.com>
2015-09-28 01:23:15 -0300

    Ajustando Querydsl

--- src/main/java/br/com/adrianohardcore/controller/FornecedorController.java
+++ src/main/java/br/com/adrianohardcore/controller/FornecedorController.java
@@ -1,18 +1,20 @@
 package br.com.adrianohardcore.controller;
 
 
-import br.com.adrianohardcore.model.Cliente;
 import br.com.adrianohardcore.model.Fornecedor;
 import br.com.adrianohardcore.model.QFornecedor;
 import br.com.adrianohardcore.repository.FornecedorRepository;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mysema.query.BooleanBuilder;
-import com.mysema.query.types.Predicate;
-import org.apache.xpath.functions.FuncConcat;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,259 +22,98 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.data.domain.Sort.Direction;
-
-import org.springframework.core.annotation.Order;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-
-
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Direction;
-import org.apache.commons.lang3.StringUtils;
 
 
 @Controller
 @PreAuthorize("hasRole('ADMIN')")
 public class FornecedorController {
-	private final Logger log = LoggerFactory.getLogger(this.getClass());
-	
-	@Autowired    
-	public FornecedorRepository fornecedorRepository;
-	
- 	@RequestMapping(value = "/fornecedor", method = RequestMethod.GET)	
-	public String lista(){				
-		return "/fornecedor/lista";
-	}
-    
-    @RequestMapping(value = "/fornecedores",method = RequestMethod.GET, produces="application/json")
-	@ResponseBody
-	public Map<String,Object> findItems(
-        //return fornecedorRepository.findAll(
-			@RequestParam(value="search",required=false) String search,
-			@RequestParam(value="sort",required=false) String psort,
-			@RequestParam(value="order",required=false) String porder,
-			@RequestParam(value="limit",required=false) Integer limit,
-			@RequestParam(value="offset",required=false) Integer offset,
-            @RequestParam(value="filter",required=false) String filter
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-			
-		)
-		{
-		Integer page = (offset / limit) ;
+    @Autowired
+    public FornecedorRepository fornecedorRepository;
 
-            log.info("filter: " + filter);
+    @RequestMapping(value = "/fornecedor", method = RequestMethod.GET)
+    public String lista() {
+        return "/fornecedor/lista";
+    }
 
-            Map<String,String> example = jsonToMap(filter);
-
-
-            //{"nomefornecedor":"LOJA","nomefantasia":"BARB"}
-            //{"nomefornecedor":"LOJA"}
-
-            //String[] campo = filter.split('',");
-
-
-
-
-		//Order order = new Order(Direction.fromString(psort.toLowerCase()),porder);	
-		
-		//Sort sort = new Sort(porder);
-
-
-
-			
-			
-	    Pageable pageRequest = new PageRequest( page,limit,Direction.fromString(porder),psort);
-		//Pageable pageRequest = new PageRequest( page,limit,Sort.Direction.DESC,psort);
-		log.info("limit: " + limit.toString() + " offset: " + offset.toString() + " totalPages: " + page + " order: " + porder + " psort: " + psort + " search: " + search);
-		Page<Fornecedor> fornecedores;
-
-            QFornecedor fornecedor = QFornecedor.fornecedor;
-            BooleanBuilder where = new BooleanBuilder();
-            if (search != null) {
-                if (filter != null){
-                    for (Map.Entry<String, String> entry : example.entrySet())
-                    {
-                        log.info("coluna: " + entry.getKey() + " valor: " + entry.getValue());
-                        if (entry.getKey() == "idfornecedor")
-                            where.and(fornecedor.idfornecedor.eq(Integer.parseInt(entry.getValue())));
-                        if (entry.getKey() == "nomefornecedor")
-                            where.and(fornecedor.nomefornecedor.equalsIgnoreCase(entry.getValue()));
-                        if (entry.getKey() == "nomefantasia")
-                            where.and(fornecedor.nomefantasia.equalsIgnoreCase(entry.getValue()));
-                    }
+    @RequestMapping(value = "/fornecedores", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Map<String, Object> findItems(
+            @RequestParam(value = "search", required = false) String search,
+            @RequestParam(value = "sort", required = false) String psort,
+            @RequestParam(value = "order", required = false) String porder,
+            @RequestParam(value = "limit", required = false) Integer limit,
+            @RequestParam(value = "offset", required = false) Integer offset,
+            @RequestParam(value = "filter", required = false) String filter
+    ) {
+        Integer page = (offset / limit);
+        log.info("filter: " + filter);
+        Map<String, String> example = jsonToMap(filter);
+        Pageable pageRequest = new PageRequest(page, limit, Direction.fromString(porder), psort);
+        log.info("limit: " + limit.toString() + " offset: " + offset.toString() + " totalPages: " + page + " order: " + porder + " psort: " + psort + " search: " + search);
+        QFornecedor fornecedor = QFornecedor.fornecedor;
+        BooleanBuilder where = new BooleanBuilder();
+        if (search != null) {
+            if (filter != null) {
+                for (Map.Entry<String, String> entry : example.entrySet()) {
+                    log.info("coluna: " + entry.getKey() + " valor: " + entry.getValue());
+                    if (entry.getKey() == "idfornecedor" && StringUtils.isNumeric(entry.getValue()))
+                        where.or(fornecedor.idfornecedor.eq(Integer.parseInt(entry.getValue())));
+                    if (entry.getKey() == "nomefornecedor")
+                        where.or(fornecedor.nomefornecedor.containsIgnoreCase(entry.getValue()));
+                    if (entry.getKey() == "nomefantasia")
+                        where.or(fornecedor.nomefantasia.containsIgnoreCase(entry.getValue()));
                 }
-                else {
-                    where.or(fornecedor.nomefornecedor.containsIgnoreCase(search));
-                    where.or(fornecedor.nomefantasia.containsIgnoreCase(search));
-                }
+            } else {
+                if (StringUtils.isNumeric(search))
+                    where.or(fornecedor.idfornecedor.eq(Integer.parseInt(search)));
+                where.or(fornecedor.nomefornecedor.containsIgnoreCase(search));
+                where.or(fornecedor.nomefantasia.containsIgnoreCase(search));
             }
-            fornecedores = fornecedorRepository.findAll(where,pageRequest);
-		
-//		if (StringUtils.isEmpty(search) ){
-//			fornecedores = fornecedorRepository.findAll(pageRequest);
-            //fornecedores = fornecedorRepository.fullTextSearch("%" + search + "%",pageRequest);
-//		}
-//		else
-	//	{
-
-
-
-
-
-
-
-
-			//users = userRepository.findByNameLike("%" + search + "%",pageRequest);
-			//fornecedores = fornecedorRepository.fullTextSearch("%" + search + "%",pageRequest);
-			//fornecedores = fornecedorRepository.findByNomefornecedorLikeIgnoreCaseOrNomefantasiaLikeIgnoreCase("%" + search + "%", "%" + search + "%",pageRequest);
-            //fornecedores = fornecedorRepository.fullTextSearch("%" + search + "%",pageRequest);
-
-            //QFornecedor fornecedor = QFornecedor.fornecedor;
-            //Predicate consulta1 =  //fornecedor.nomefornecedor.like(search);
-            //fornecedores = fornecedorRepository.findAll(fornecedor.nomefornecedor.containsIgnoreCase(search).or(fornecedor.nomefantasia.containsIgnoreCase(search)),pageRequest);
-
-            //Predicate predicate =  fornecedor.nomefornecedor.containsIgnoreCase(search);
-            //predicate = fornecedor.nomefantasia.containsIgnoreCase(search);
-            //fornecedores = fornecedorRepository.findAll(predicate,pageRequest);
-
-//            QFornecedor fornecedor = QFornecedor.fornecedor;
-//            BooleanBuilder where = new BooleanBuilder();
-//            if (search != null) {
-//                where.or(fornecedor.nomefornecedor.containsIgnoreCase(search));
-//                where.or(fornecedor.nomefantasia.containsIgnoreCase(search));
-//            }
-//            fornecedores = fornecedorRepository.findAll(where,pageRequest);
-//            if (lastName != null) {
-//                where.and(employee.lastName.eq(lastName));
-//            }
-
-
-
-            //QUser user = QUser.user;
-            //Page<User> page =  userRepository.findAll(user.name.like(name).and(user.age.eq(age)), new PageRequest(0,10));
-
-             //.endsWith("z");
-            //fornecedores = fornecedorRepository.findAll(consulta1,pageRequest);
-
-
-
-
-			//log.info("pesquisa: " + search + " registros: " + fornecedores.getTotalElements() );
-//		}
-		
-
-		
-	
-		Map<String,Object> modelMap = new HashMap<String,Object>();
-		modelMap.put("total", fornecedores.getTotalElements());
-		modelMap.put("rows", fornecedores.getContent());
+        }
+        Page<Fornecedor> fornecedores = fornecedorRepository.findAll(where, pageRequest);
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        modelMap.put("total", fornecedores.getTotalElements());
+        modelMap.put("rows", fornecedores.getContent());
         return modelMap;
     }
-	
-		// @Transactional
-	// @RequestMapping(value="/userlist", produces="application/json")
-	// public @ResponseBody Map<String,Object> listJqGrid(
-			// @RequestParam("_search") Boolean search,
-    		// @RequestParam(value="filters", required=false) String filters,
-    		// @RequestParam(value="page", required=false) Integer page,
-    		// @RequestParam(value="rows", required=false) Integer rows,
-    		// @RequestParam(value="sidx", required=false) String sidx,
-    		// @RequestParam(value="sord", required=false) String sord	
-	
-	
-	
 
-	@RequestMapping(value = "/fornecedor/form", method = RequestMethod.GET)
-	public String form(Model model){
-		log.info("Formulario de cadastro de fornecedor");
-		model.addAttribute("fornecedor", new Fornecedor());
-		return "/fornecedor/form";
-	}
+    @RequestMapping(value = "/fornecedor/form", method = RequestMethod.GET)
+    public String form(Model model) {
+        log.info("Formulario de cadastro de fornecedor");
+        model.addAttribute("fornecedor", new Fornecedor());
+        return "/fornecedor/form";
+    }
 
-	
-/*     @RequestMapping(value="/fornecedor/form", method=RequestMethod.POST)
-    public String formSave(@ModelAttribute Fornecedor fornecedor, Model model) {
+    @Transactional
+    @RequestMapping(value = "/fornecedor/form", method = RequestMethod.POST)
+    public String formSave(@Valid Fornecedor fornecedor, BindingResult result) {
+        if (result.hasErrors())
+            return "/fornecedor/form";
         fornecedorRepository.save(fornecedor);
-        return "result";
-    } */	
-	
-/* 	@RequestMapping(value = "/fornecedor/form", method = RequestMethod.POST)
-	public String formSave(@Valid Fornecedor fornecedor, BindingResult result) {		
- 		if (result.hasErrors())
-			return "/fornecedor/form";
-		//fornecedorRepository.save(fornecedor); 
-		return "redirect:/fornecedor";
-	} */	
-	
-	
-/* @ModelAttribute("timesheet") TimeSheet timeSheet, */
+        return "redirect:/fornecedor";
+    }
 
-	@Transactional
-	@RequestMapping(value = "/fornecedor/form", method = RequestMethod.POST)
-	public String formSave(@Valid Fornecedor fornecedor, BindingResult result) {		
- 		if (result.hasErrors())
-			return "/fornecedor/form";
-		fornecedorRepository.save(fornecedor);
-		return "redirect:/fornecedor";
-	}
-
-    private Map<String, String> jsonToMap(String json){
+    private Map<String, String> jsonToMap(String json) {
         //String json = "{\"name\":\"mkyong\", \"age\":\"29\"}";
-
-        Map<String,String> map = new HashMap<String,String>();
+        Map<String, String> map = new HashMap<String, String>();
         ObjectMapper mapper = new ObjectMapper();
         try {
             //convert JSON string to Map
             map = mapper.readValue(json,
-                    new TypeReference<HashMap<String,String>>(){});
+                    new TypeReference<HashMap<String, String>>() {
+                    });
             System.out.println(map);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return map;
-
     }
-	
-	
-	
-	
-
 }
--- src/main/java/br/com/adrianohardcore/repository/FornecedorRepository.java
+++ src/main/java/br/com/adrianohardcore/repository/FornecedorRepository.java
@@ -13,26 +13,19 @@ import java.util.function.Predicate;
 
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer>, QueryDslPredicateExecutor<Fornecedor> {
 
-	Page<Fornecedor> findAll(Pageable pageRequest);
-	Page<Fornecedor> findByNomefornecedorLike(String name, Pageable pageRequest);
-	Page<Fornecedor> findByNomefantasiaLike(String name, Pageable pageRequest);
-	Page<Fornecedor> findByNomefornecedorOrNomefantasia(String name, Pageable pageRequest);
+    Page<Fornecedor> findAll(Pageable pageRequest);
 
+    Page<Fornecedor> findByNomefornecedorLike(String name, Pageable pageRequest);
+
+    Page<Fornecedor> findByNomefantasiaLike(String name, Pageable pageRequest);
+
+    Page<Fornecedor> findByNomefornecedorOrNomefantasia(String name, Pageable pageRequest);
 
     Page<Fornecedor> findAll(Predicate name, Pageable pageRequest);
 
-	Page<Fornecedor> findByNomefornecedorLikeIgnoreCaseOrNomefantasiaLikeIgnoreCase(String query1, String query2, Pageable pageable);
+    Page<Fornecedor> findByNomefornecedorLikeIgnoreCaseOrNomefantasiaLikeIgnoreCase(String query1, String query2, Pageable pageable);
 
-	//@Query(value = "select f from Fornecedor f where f.nomefornecedor like upper(%:text%) or f.nomefantasia like upper(%:text%)")
-
-    @Query(value="SELECT f FROM Fornecedor f WHERE " +
+    @Query(value = "SELECT f FROM Fornecedor f WHERE " +
             "(?1 IS NULL  OR UPPER(f.nomefornecedor) LIKE UPPER(CONCAT('%',:text, '%'))  )")
-
-            //"UPPER(f.nomefornecedor) LIKE UPPER(CONCAT('%',:text, '%')) OR " +
-            //"UPPER(f.nomefantasia) LIKE UPPER(CONCAT('%',:text, '%'))")
     Page<Fornecedor> fullTextSearch(@Param("text") String text, Pageable pageable);
-
-
-
-
 }
