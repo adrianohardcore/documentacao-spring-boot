5fa44fed2edbc3db2cef1932bcaa221a66411dc7
Adrino Faria Alves <adriano.faria@gmail.com>
2015-09-28 00:39:32 -0300

    Adicionando suporte ao QueryDsl para pesquisas avançadas.

--- src/main/java/br/com/adrianohardcore/controller/FornecedorController.java
+++ src/main/java/br/com/adrianohardcore/controller/FornecedorController.java
@@ -3,7 +3,13 @@ package br.com.adrianohardcore.controller;
 
 import br.com.adrianohardcore.model.Cliente;
 import br.com.adrianohardcore.model.Fornecedor;
+import br.com.adrianohardcore.model.QFornecedor;
 import br.com.adrianohardcore.repository.FornecedorRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mysema.query.BooleanBuilder;
+import com.mysema.query.types.Predicate;
+import org.apache.xpath.functions.FuncConcat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,6 +59,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -82,15 +89,32 @@ public class FornecedorController {
 			@RequestParam(value="sort",required=false) String psort,
 			@RequestParam(value="order",required=false) String porder,
 			@RequestParam(value="limit",required=false) Integer limit,
-			@RequestParam(value="offset",required=false) Integer offset
+			@RequestParam(value="offset",required=false) Integer offset,
+            @RequestParam(value="filter",required=false) String filter
+
 			
 		)
 		{
 		Integer page = (offset / limit) ;
-		
+
+            log.info("filter: " + filter);
+
+            Map<String,String> example = jsonToMap(filter);
+
+
+            //{"nomefornecedor":"LOJA","nomefantasia":"BARB"}
+            //{"nomefornecedor":"LOJA"}
+
+            //String[] campo = filter.split('',");
+
+
+
+
 		//Order order = new Order(Direction.fromString(psort.toLowerCase()),porder);	
 		
-		//Sort sort = new Sort(porder);	    
+		//Sort sort = new Sort(porder);
+
+
 
 			
 			
@@ -98,26 +122,87 @@ public class FornecedorController {
 		//Pageable pageRequest = new PageRequest( page,limit,Sort.Direction.DESC,psort);
 		log.info("limit: " + limit.toString() + " offset: " + offset.toString() + " totalPages: " + page + " order: " + porder + " psort: " + psort + " search: " + search);
 		Page<Fornecedor> fornecedores;
+
+            QFornecedor fornecedor = QFornecedor.fornecedor;
+            BooleanBuilder where = new BooleanBuilder();
+            if (search != null) {
+                if (filter != null){
+                    for (Map.Entry<String, String> entry : example.entrySet())
+                    {
+                        log.info("coluna: " + entry.getKey() + " valor: " + entry.getValue());
+                        if (entry.getKey() == "idfornecedor")
+                            where.and(fornecedor.idfornecedor.eq(Integer.parseInt(entry.getValue())));
+                        if (entry.getKey() == "nomefornecedor")
+                            where.and(fornecedor.nomefornecedor.equalsIgnoreCase(entry.getValue()));
+                        if (entry.getKey() == "nomefantasia")
+                            where.and(fornecedor.nomefantasia.equalsIgnoreCase(entry.getValue()));
+                    }
+                }
+                else {
+                    where.or(fornecedor.nomefornecedor.containsIgnoreCase(search));
+                    where.or(fornecedor.nomefantasia.containsIgnoreCase(search));
+                }
+            }
+            fornecedores = fornecedorRepository.findAll(where,pageRequest);
 		
-		if (StringUtils.isEmpty(search) ){			
-			fornecedores = fornecedorRepository.findAll(pageRequest);
-		}
-		else
-		{ 
-			log.info("Pesquisando...");
+//		if (StringUtils.isEmpty(search) ){
+//			fornecedores = fornecedorRepository.findAll(pageRequest);
+            //fornecedores = fornecedorRepository.fullTextSearch("%" + search + "%",pageRequest);
+//		}
+//		else
+	//	{
+
+
+
+
+
+
+
+
 			//users = userRepository.findByNameLike("%" + search + "%",pageRequest);
-			fornecedores = fornecedorRepository.fullTextSearch("%" + search + "%",pageRequest);
+			//fornecedores = fornecedorRepository.fullTextSearch("%" + search + "%",pageRequest);
 			//fornecedores = fornecedorRepository.findByNomefornecedorLikeIgnoreCaseOrNomefantasiaLikeIgnoreCase("%" + search + "%", "%" + search + "%",pageRequest);
             //fornecedores = fornecedorRepository.fullTextSearch("%" + search + "%",pageRequest);
-			log.info("pesquisa: " + search + " registros: " + fornecedores.getTotalElements() );
-		}
+
+            //QFornecedor fornecedor = QFornecedor.fornecedor;
+            //Predicate consulta1 =  //fornecedor.nomefornecedor.like(search);
+            //fornecedores = fornecedorRepository.findAll(fornecedor.nomefornecedor.containsIgnoreCase(search).or(fornecedor.nomefantasia.containsIgnoreCase(search)),pageRequest);
+
+            //Predicate predicate =  fornecedor.nomefornecedor.containsIgnoreCase(search);
+            //predicate = fornecedor.nomefantasia.containsIgnoreCase(search);
+            //fornecedores = fornecedorRepository.findAll(predicate,pageRequest);
+
+//            QFornecedor fornecedor = QFornecedor.fornecedor;
+//            BooleanBuilder where = new BooleanBuilder();
+//            if (search != null) {
+//                where.or(fornecedor.nomefornecedor.containsIgnoreCase(search));
+//                where.or(fornecedor.nomefantasia.containsIgnoreCase(search));
+//            }
+//            fornecedores = fornecedorRepository.findAll(where,pageRequest);
+//            if (lastName != null) {
+//                where.and(employee.lastName.eq(lastName));
+//            }
+
+
+
+            //QUser user = QUser.user;
+            //Page<User> page =  userRepository.findAll(user.name.like(name).and(user.age.eq(age)), new PageRequest(0,10));
+
+             //.endsWith("z");
+            //fornecedores = fornecedorRepository.findAll(consulta1,pageRequest);
+
+
+
+
+			//log.info("pesquisa: " + search + " registros: " + fornecedores.getTotalElements() );
+//		}
 		
 
 		
 	
 		Map<String,Object> modelMap = new HashMap<String,Object>();
 		modelMap.put("total", fornecedores.getTotalElements());
-		modelMap.put("rows", fornecedores.getContent());		
+		modelMap.put("rows", fornecedores.getContent());
         return modelMap;
     }
 	
@@ -166,7 +251,25 @@ public class FornecedorController {
 			return "/fornecedor/form";
 		fornecedorRepository.save(fornecedor);
 		return "redirect:/fornecedor";
-	}		
+	}
+
+    private Map<String, String> jsonToMap(String json){
+        //String json = "{\"name\":\"mkyong\", \"age\":\"29\"}";
+
+        Map<String,String> map = new HashMap<String,String>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            //convert JSON string to Map
+            map = mapper.readValue(json,
+                    new TypeReference<HashMap<String,String>>(){});
+            System.out.println(map);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return map;
+
+    }
 	
 	
 	
--- src/main/java/br/com/adrianohardcore/repository/FornecedorRepository.java
+++ src/main/java/br/com/adrianohardcore/repository/FornecedorRepository.java
@@ -5,22 +5,33 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
-public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer>{
+
+import java.util.function.Predicate;
+
+
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer>, QueryDslPredicateExecutor<Fornecedor> {
+
 	Page<Fornecedor> findAll(Pageable pageRequest);
 	Page<Fornecedor> findByNomefornecedorLike(String name, Pageable pageRequest);
 	Page<Fornecedor> findByNomefantasiaLike(String name, Pageable pageRequest);
 	Page<Fornecedor> findByNomefornecedorOrNomefantasia(String name, Pageable pageRequest);
 
+
+    Page<Fornecedor> findAll(Predicate name, Pageable pageRequest);
+
 	Page<Fornecedor> findByNomefornecedorLikeIgnoreCaseOrNomefantasiaLikeIgnoreCase(String query1, String query2, Pageable pageable);
-	
+
 	//@Query(value = "select f from Fornecedor f where f.nomefornecedor like upper(%:text%) or f.nomefantasia like upper(%:text%)")
 
-    @Query("SELECT f FROM Fornecedor f WHERE " +
-            "UPPER(f.nomefornecedor) LIKE UPPER(CONCAT('%',:text, '%')) OR " +
-            "UPPER(f.nomefantasia) LIKE UPPER(CONCAT('%',:text, '%'))")
+    @Query(value="SELECT f FROM Fornecedor f WHERE " +
+            "(?1 IS NULL  OR UPPER(f.nomefornecedor) LIKE UPPER(CONCAT('%',:text, '%'))  )")
+
+            //"UPPER(f.nomefornecedor) LIKE UPPER(CONCAT('%',:text, '%')) OR " +
+            //"UPPER(f.nomefantasia) LIKE UPPER(CONCAT('%',:text, '%'))")
     Page<Fornecedor> fullTextSearch(@Param("text") String text, Pageable pageable);
-	
+
 
 
 
